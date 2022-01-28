ae6ad28c65648a54cbedc3bd77a2a23e9d7ab8cf
caio_eng.ambiental@hotmail.com <caio_eng.ambiental@hotmail.com>
2022-01-28 00:47:15 -0300

    Simple Create form

--- VendasWebMvc/Controllers/SellersController.cs
+++ VendasWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VendasWebMvc.Models;
 using VendasWebMvc.Services;
 
 namespace VendasWebMvc.Controllers
@@ -19,6 +20,19 @@ namespace VendasWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Para evitar ataques CSRF
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
