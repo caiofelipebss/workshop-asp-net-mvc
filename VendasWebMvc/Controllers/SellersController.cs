37af96efa8ef816896a259561487ee8391519dca
caio_eng.ambiental@hotmail.com <caio_eng.ambiental@hotmail.com>
2022-01-28 23:53:20 -0300

    Update seller and custom service exception

--- VendasWebMvc/Controllers/SellersController.cs
+++ VendasWebMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using VendasWebMvc.Models;
 using VendasWebMvc.Models.ViewModels;
 using VendasWebMvc.Services;
+using VendasWebMvc.Services.Exceptions;
 
 namespace VendasWebMvc.Controllers
 {
@@ -82,6 +83,46 @@ namespace VendasWebMvc.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- VendasWebMvc/Services/SellerService.cs
+++ VendasWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using VendasWebMvc.Data;
 using VendasWebMvc.Models;
+using VendasWebMvc.Services.Exceptions;
 
 namespace VendasWebMvc.Services
 {
@@ -39,6 +40,25 @@ namespace VendasWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id)) //Vê se não existe no banco de dados algum vendededor X com o ID igual ao ID.obj
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+            _context.Update(obj);
+            _context.SaveChanges();
+            }
+
+            //Se uma exceção de nível de acesso a dados acontecer, lançará uma exceção da camada de serviços. Assim o controlador só conversará apenas com a camada de serviço.
+            catch (DbUpdateConcurrencyException e) 
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
