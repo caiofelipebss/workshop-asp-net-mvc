bda3c6b90d79f07531040bf430a865b54242a696
caio_eng.ambiental@hotmail.com <caio_eng.ambiental@hotmail.com>
2022-01-29 02:03:15 -0300

    Asynchronous operations using Tasks (async, await)

--- VendasWebMvc/Services/SellerService.cs
+++ VendasWebMvc/Services/SellerService.cs
@@ -19,39 +19,40 @@ namespace VendasWebMvc.Services
         }
 
         // Retorna uma lista com todos os vendedores do BD
-        public List<Seller> FindAll() 
+        public async Task<List<Seller>> FindAllAsync() 
         {
-            return _context.Seller.ToList();
+            return await _context.Seller.ToListAsync();
         }
 
-        public void Insert(Seller obj)
+        public async Task InsertAsync(Seller obj)
         {           
             _context.Add(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public Seller FindById(int id)
+        public async Task<Seller> FindByIdAsync(int id)
         {
-            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id); // eager loading
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id); // eager loading
         }
 
-        public void Remove(int id)
+        public async Task RemoveAsync(int id)
         {
-            var obj = _context.Seller.Find(id);
+            var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public void Update(Seller obj)
+        public async Task UpdateAsync(Seller obj)
         {
-            if (!_context.Seller.Any(x => x.Id == obj.Id)) //Vê se não existe no banco de dados algum vendededor X com o ID igual ao ID.obj
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny) //Vê se não existe no banco de dados algum vendededor X com o ID igual ao ID.obj
             {
                 throw new NotFoundException("Id not found");
             }
             try
             {
             _context.Update(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             }
 
             //Se uma exceção de nível de acesso a dados acontecer, lançará uma exceção da camada de serviços. Assim o controlador só conversará apenas com a camada de serviço.
